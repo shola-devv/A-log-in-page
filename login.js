44af15adb3ff62dfe9e817056812f199ab48cab2
shola emmanuel <creekemmanuelf@gmail.com>
2024-02-27 17:54:41 +0100

    updating this repo

---
+++ login.js
@@ -0,0 +1,75 @@
+function wowo(){
+ let wop = document.getElementById('coc')
+  
+}
+ let roop = {
+  hoot: "fghj",
+  noot: (poo)=> {
+    return poo
+  }
+ }
+
+ console.log(roop.noot("loo"))
+
+
+
+ const wowa = {
+  tool: 4,
+  yool: [],
+  wowp : ()=>{
+    for (let p = 0; p < 10; p++)
+  
+  console.log(tox)
+  }
+ }
+ wowa.wowp
+
+
+
+ function tool (wow, eooo){
+  this.wow = wow;
+  this.eooo = eooo;
+ 
+ }
+
+let woz = new tool(2, 7);
+console.log(woz.eooo)
+tool.prototype.nety = ()=>{
+  console.log("works")
+}
+woz.nety();
+let thrill = window.location.pathname
+console.log(thrill);
+let uio = (uoo)=> {
+  let tiup = uoo - 1;
+  console.log(uoo)
+
+  if (tiup > 0){
+    uio((tiup))
+  }
+}
+uio(7)
+
+function wppp(){
+  let eeee = window.location.reload();
+  
+  alert("ghjk")
+  return eeee
+}
+document.getElementById("firstb").onclick = wppp;
+let eroo = 0
+ let message = (eroo > 9) ? (alert('ghj')) : (alert('fghj'))
+
+
+
+ class person {
+  constructor(food, name, school){
+   this.food = food,
+   this.name = name,
+   this.school = school
+  }
+ }
+
+ let news = new person("name", "fgh", "fghj")
+let newt = news.flat();
+ console.log(newt)
